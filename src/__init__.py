af3c28e932713742d55fc91f2510e2c6de44a8b5
llc1995@sina.com <llc1995@sina.com>
2023-07-18 17:55:18 +0800

    optimize inference

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 from .dataset import MIR1K, MIR_ST500, MDB
 from .constants import *
 from .model import E2E, E2E0
-from .utils import cycle, summary, to_local_average_cents, to_viterbi_cents
+from .utils import cycle, summary, to_local_average_cents, to_local_average_f0, to_viterbi_cents, to_viterbi_f0
 from .loss import FL, bce, smoothl1
 from .inference import RMVPE
 from .spec import MelSpectrogram
--- src/model.py
+++ src/model.py
@@ -7,10 +7,9 @@ from .seq import BiGRU
 
 
 class E2E(nn.Module):
-    def __init__(self, hop_length, n_blocks, n_gru, kernel_size, en_de_layers=5, inter_layers=4, in_channels=1,
+    def __init__(self, n_blocks, n_gru, kernel_size, en_de_layers=5, inter_layers=4, in_channels=1,
                  en_out_channels=16):
         super(E2E, self).__init__()
-        self.mel = MelSpectrogram(N_MELS, SAMPLE_RATE, WINDOW_LENGTH, hop_length, None, MEL_FMIN, MEL_FMAX)
         self.unet = DeepUnet(kernel_size, n_blocks, en_de_layers, inter_layers, in_channels, en_out_channels)
         self.cnn = nn.Conv2d(en_out_channels, 3, (3, 3), padding=(1, 1))
         if n_gru:
@@ -27,17 +26,11 @@ class E2E(nn.Module):
                 nn.Sigmoid()
             )
 
-    def forward(self, x):
-        mel = self.mel(x.reshape(-1, x.shape[-1])).transpose(-1, -2).unsqueeze(1)
+    def forward(self, mel):
+        mel = mel.transpose(-1, -2).unsqueeze(1)
         x = self.cnn(self.unet(mel)).transpose(1, 2).flatten(-2)
-        # x = self.fc(x)
-        hidden_vec = 0
-        if len(self.fc) == 4:
-            for i in range(len(self.fc)):
-                x = self.fc[i](x)
-                if i == 0:
-                    hidden_vec = x
-        return hidden_vec, x
+        x = self.fc(x)
+        return x
 
 
 class E2E0(nn.Module):
